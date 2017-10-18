4c399a0c3e9afee4fc956e7e3fba3b53a982abb7
Mark <mark.torrey@gmail.com>
2017-10-18 14:50:32 -0400

    clean up files dir

--- files/putSchemas.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-# curl -XPUT -H 'content-type: application/json' koop.com/nad/addresses @fullDataset.json
-# curl -XPUT -H 'content-type: application/json' koop.com/nad/addresses/florida @oneSchema.json

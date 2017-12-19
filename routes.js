cb7cb01d268b21fa7ab14cc242b6175d6835ad7d
dmfenton <dfenton@esri.com>
2017-12-19 12:12:25 -0500

    reformat routes

--- routes.js
+++ routes.js
@@ -4,14 +4,15 @@
   This file is an optional place to specify additional routes to be handled by this provider's controller
   Documentation: http://koopjs.github.io/docs/specs/provider/
 */
-module.exports = [{
-  path: '/composite/:id',
-  methods: ['get', 'put', 'delete', 'post'],
-  handler: 'dataset'
-},
-{
-  path: '/composite/:id/:schema',
-  methods: ['get', 'put', 'delete'],
-  handler: 'datasetSchema'
-}
+module.exports = [
+  {
+    path: '/composite/:id',
+    methods: ['get', 'put', 'delete', 'post'],
+    handler: 'dataset'
+  },
+  {
+    path: '/composite/:id/:schema',
+    methods: ['get', 'put', 'delete'],
+    handler: 'datasetSchema'
+  }
 ]
